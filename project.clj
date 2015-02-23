6c4e9f82fcb7817136a53d19d7b5a8e8efb20706
Peter Gillard-Moss <peter@gillardmoss.me.uk>
2015-02-23 15:21:22 +0000

    Handle windows paths s3-sync is upgraded with fix for windows paths

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lein-s3-static-deploy "0.1.0"
+(defproject lein-s3-static-deploy "0.1.1-SNAPSHOT"
   :description "Leiningen plugin to deploy static websites"
   :url "https://github.com/ThoughtWorksInc/lein-s3-static-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-aws-s3 "0.3.7"]
-                 [me.kanej/s3-sync "0.2.0"]
+                 [me.kanej/s3-sync "0.4.0"]
                  [org.clojure/java.data "0.1.1"]]
   :eval-in-leiningen true
   :min-lein-version "2.0.0"
